6551ea9753bb2f21b6d9d6994634df4d98560d43
Rakka Alhazimi <rakkakeren@gmail.com>
2026-02-05 00:54:28 +0700

    changed: space after include

--- VtuberAppVS/src/main.cpp
+++ VtuberAppVS/src/main.cpp
@@ -16,9 +16,9 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include<imgui/imgui.h>
-#include<imgui/imgui_impl_glfw.h>
-#include<imgui/imgui_impl_opengl3.h>
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
 
 #include "onnxruntime_cxx_api.h"
 #include <opencv2/opencv.hpp>
