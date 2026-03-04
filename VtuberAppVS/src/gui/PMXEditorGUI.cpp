8da47833987e9eeb6d4eb4e1a4b241d9dbdd6096
Rakka Alhazimi <rakkakeren@gmail.com>
2026-03-04 22:44:19 +0700

    added: field input for rotation and position

--- VtuberAppVS/src/gui/PMXEditorGUI.cpp
+++ VtuberAppVS/src/gui/PMXEditorGUI.cpp
@@ -52,21 +52,41 @@ void PMXEditorGUI::draw()
         // previousRotation = boneRotation;
         bool isSliderXActive = ImGui::SliderFloat("rotation-x", &currentBoneRotation.x, -glm::pi<float>(), glm::pi<float>(), "%.3f");
         ImGui::SameLine();
-        ImGui::InputFloat("##rotation-x", &currentBoneRotation.x, 0.01f, 0.1f, "%.3f");
+        bool isInputXActive = ImGui::InputFloat("##rotation-x", &currentBoneRotation.x, 0.01f, 0.1f, "%.3f");
+
         bool isSliderYActive = ImGui::SliderFloat("rotation-y", &currentBoneRotation.y, -glm::pi<float>(), glm::pi<float>(), "%.3f");
+        ImGui::SameLine();
+        bool isInputYActive = ImGui::InputFloat("##rotation-y", &currentBoneRotation.y, 0.01f, 0.1f, "%.3f");
+
         bool isSliderZActive = ImGui::SliderFloat("rotation-z", &currentBoneRotation.z, -glm::pi<float>(), glm::pi<float>(), "%.3f");
+        ImGui::SameLine();
+        bool isInputZActive = ImGui::InputFloat("##rotation-z", &currentBoneRotation.z, 0.01f, 0.1f, "%.3f");
         
-        if ((isSliderXActive || isSliderYActive || isSliderZActive))
+        if (
+          isSliderXActive || isSliderYActive || isSliderZActive || 
+          isInputXActive || isInputYActive || isInputZActive
+          )
         {
           RotateBoneCommand command(model, (int)i, currentBoneRotation);
           command.execute();
         }
 
         bool isPositionXActive = ImGui::SliderFloat("position-x", &currentBonePosition.x, -10.0f, 10.0f, "%.3f");
-        bool isPositionYActive = ImGui::SliderFloat("position-y", &currentBonePosition.y, -10.0f, 10.0f, "%.3f");
-        bool isPositionZActive = ImGui::SliderFloat("position-z", &currentBonePosition.z, -10.0f, 10.0f, "%.3f");
+        ImGui::SameLine();
+        bool isInputPositionXActive = ImGui::InputFloat("##position-x", &currentBonePosition.x, 0.01f, 0.1f, "%.3f");
 
-        if ((isPositionXActive || isPositionYActive || isPositionZActive))
+        bool isPositionYActive = ImGui::SliderFloat("position-y", &currentBonePosition.y, -10.0f, 10.0f, "%.3f");
+        ImGui::SameLine();
+        bool isInputPositionYActive = ImGui::InputFloat("##position-y", &currentBonePosition.y, 0.01f, 0.1f, "%.3f");
+
+        bool isPositionZActive = ImGui::SliderFloat("position-z", &currentBonePosition.z, -10.0f, 10.0f, "%.3f");
+        ImGui::SameLine();
+        bool isInputPositionZActive = ImGui::InputFloat("##position-z", &currentBonePosition.z, 0.01f, 0.1f, "%.3f");
+
+        if (
+          isPositionXActive || isPositionYActive || isPositionZActive ||
+          isInputPositionXActive || isInputPositionYActive || isInputPositionZActive
+          )
         {
           TranslateBoneCommand command(model, (int)i, currentBonePosition);
           command.execute();
